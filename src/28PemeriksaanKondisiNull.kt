9f2db9572441bd6118d9d857f1632d240bc48485
roisulfikri <roisulfikri8@gmail.com>
2025-09-20 09:57:31 +0700

    Percobaan Pemeriksaaan Kondisi Null

---
+++ src/28PemeriksaanKondisiNull.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var mayNumber: Int? = 15
+    if (mayNumber != null)
+        println(mayNumber * 2)
+    else
+        println("The entered value isn't a number")
+}
