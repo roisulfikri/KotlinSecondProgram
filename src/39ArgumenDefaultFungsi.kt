3b2ae2c6e1a644d3f7dd0aa5dac95d6d6dd7ca98
roisulfikri <roisulfikri8@gmail.com>
2025-09-26 08:50:53 +0700

    Percobaan Argumen Default Fungsi

---
+++ src/39ArgumenDefaultFungsi.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    connectToDb()
+    println()
+
+    connectToDb("sqlserver", "depandi")
+}
+fun connectToDb(hostname: String = "localhost",
+                username: String = "mysql",
+                password:String = "secret") {
+    println("hostname : $hostname")
+    println("username : $username")
+    println("password : $password")
+}
